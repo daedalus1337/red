039f4b004d2902502a739d4ee486b262062de666
daedalus1337 <88613690+daedalus1337@users.noreply.github.com>
2021-08-08 06:44:13 -0400

    Update red.py

--- red.py
+++ red.py
@@ -6,7 +6,7 @@ url = "https://redacted.ch/ajax.php?"
 header = {"Authorization": apikey}
 
 def sizeof_fmt(num, suffix='B'):
-	for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+	for unit in ['','K','M','G','T','P','E','Z']:
 		if abs(num) < 1024.0:
 			return "%3.1f%s%s" % (num, unit, suffix)
 		num /= 1024.0
