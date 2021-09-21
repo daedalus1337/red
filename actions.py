014500e78119ef9fe4f81e063183fffd383a41c6
Alexandre Pinto <senhoralexandrepinto@protonmail.com>
2021-09-21 13:34:31 +0100

    cleans torrent_download() 🧹
    the if/else on fl boolean was removed; it is now a little cleaner.
    path is now a separate variable, and there's a confirmation message on
    download success! 🎊

--- actions.py
+++ actions.py
@@ -72,20 +72,17 @@ def album_search(artist, album, media, format):
 					print("")
 
 def torrent_download(tid, fl):
-	if fl == True:
-		download_params = {"action": "download", "id": tid, "usetoken": True}
-	else:
-		download_params = {"action": "download", "id": tid}
 	details_params = {"action": "torrent", "id": tid}
 	r1 = make_request(details_params)
 	album = r1.json()["response"]["group"]["name"]
 	artist = str((r1.json()["response"]["group"]["musicInfo"]["artists"][0]["name"]))
+	download_params = {"action": "download", "id": tid, "usetoken": fl}
 	r2 = make_request(download_params)
 	open(os.getenv("FILE_DIR") + artist + " - " + album + ".torrent", "wb").write(r2.content)
 
 def user_stats():
     stats = {"action": "index"}
-    r1 = requests.get(url, params=stats, headers=header).json()["response"] 
+    r1 = requests.get(url, params=stats, headers=header).json()["response"]
     print("Username........." + r1["username"])
     print("Class............" + r1["userstats"]["class"])
     print("Ratio............" + str(r1["userstats"]["ratio"]))
